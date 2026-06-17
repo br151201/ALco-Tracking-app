bff7ee1607f3ec8571708bdab816295cb1ad09d4
br151201 <brathod200115@gmail.com>
2026-06-17 13:11:25 +0100

    Set up Express server with basic route (engine)

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+//Test route
+app.get("/",(req, res) => {
+  res.send("Parcel Tracking API is running");
+});
+
+app.listen(5000,() => console.log("Server Running on port 5000"));
